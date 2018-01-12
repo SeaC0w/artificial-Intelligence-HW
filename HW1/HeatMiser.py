02db41f79a467712866b2e5fcd83c445c6adeb1c
connellyj <juliakconnelly@gmail.com>
2018-01-12 15:08:22 -0600

    Added another condition

--- HW1/HeatMiser.py
+++ HW1/HeatMiser.py
@@ -1,16 +1,19 @@
 # HeatMiser implementation by Kerim Celik and Julia Connelly
 import random
 import numpy
-#import tabulate
+
 
 def getListAverage(ls):
     return sum(ls) / len(ls)
 
+
 def getListStdDev(ls):
     return numpy.std(ls)
 
+
 def inRange(num, opt):
-    return (num >= opt and num < opt + 1)
+    return opt <= num < opt + 1
+
 
 def main():
     # set up rooms
@@ -20,12 +23,13 @@ def main():
     optHumidDev = 1.75
     temps = []
     humids = []
-    for i in range(0,12):
-        temps.append(random.randint(65,75))
-        humids.append(random.randint(55,65))
+    for i in range(0, 12):
+        temps.append(random.randint(65, 75))
+        humids.append(random.randint(45, 55))
 
     # HeatMiser does the problem
     index = 0
+    numberOfRounds = 0
     avgTemp = getListAverage(temps)
     avgHumid = getListAverage(humids)
     stdDevTemp = getListStdDev(temps)
@@ -40,35 +44,34 @@ def main():
     print(humids)
     print("\n")
     while not (avgTempGood and avgHumidGood and stdDevTempGood and stdDevHumidGood):
-#        print(temps)
-#        print(humids)
-#        print("\n")
-        if (not (avgTempGood and stdDevTempGood)):
-            if (temps[index] < optTemp):
-                #print("a")
-                temps[index] += 1
-            elif (temps[index] >= optTemp + 1):
-                #print("b")
-                temps[index] -= 1
-#            else:
-#                if (humids[index] < optHumid):
-#                    #print("c")
-#                    humids[index] += 1
-#                elif (humids[index] >= optHumid + 1):
-#                    #print("d")
-#                    humids[index] -= 1
-        else:
-            if (humids[index] < optHumid):
-                #print("c")
-                humids[index] += 1
-            elif (humids[index] >= optHumid + 1):
-                #print("d")
-                humids[index] -= 1
+        if not (avgTempGood and stdDevTempGood):
+            if stdDevTemp < optTempDev + 0.1:
+                if avgTemp < optTemp:
+                    temps[index] += 1
+                elif avgTemp >= optTemp + 1:
+                    temps[index] -= 1
             else:
-                print("HeatMiser does nothing in room " + str(index) + ".")
+                if temps[index] < optTemp:
+                    temps[index] += 1
+                elif temps[index] >= optTemp + 1:
+                    temps[index] -= 1
+        else:
+            if stdDevHumid < optHumidDev + 0.01:
+                if avgHumid < optHumid:
+                    humids[index] += 1
+                elif avgHumid >= optHumid + 1:
+                    humids[index] -= 1
+            else:
+                if humids[index] < optHumid:
+                    humids[index] += 1
+                elif humids[index] >= optHumid + 1:
+                    humids[index] -= 1
+                else:
+                    print("HeatMiser does nothing in room " + str(index) + ".")
         index += 1
-        if (index == 12):
+        if index == 12:
             index = 0
+        numberOfRounds += 1
         avgTemp = getListAverage(temps)
         avgHumid = getListAverage(humids)
         stdDevTemp = getListStdDev(temps)
@@ -82,7 +85,7 @@ def main():
     print("\n")
     print(avgTemp)
     print(avgHumid)
-
+    print(numberOfRounds)
 
 
 if __name__ == "__main__":
